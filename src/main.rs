3b945ee34c32125eac54602a1ded317c5dff8366
Donatien Delehelle <donatien.delehelle@eyesnap.fr>
2018-08-10 14:16:32 +0200

    Ajout trait  et refactoring pour ajouter les triangles

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use rayon::prelude::*;
 use std::f32::consts::PI;
-use rand::Rng;
+// use rand::Rng;
 use rand::distributions::{IndependentSample, Range};
 
 #[derive(Debug, Clone)]
@@ -38,8 +38,32 @@ struct Sphere {
     transp: bool,
 }
 
+#[derive(Debug)]
+struct Triangle {
+    A: Vector,
+    B: Vector,
+    C: Vector,
+    albedo: Vector,
+    miroir: bool,
+    transp: bool,
+}
+
+trait Shape {
+    fn intersection(&self, r: &Ray, p: &mut Vector, n: &mut Vector, t: &mut f32) -> bool;
+
+    fn radius(&self) -> f32;
+
+    fn albedo(&self) -> Vector;
+
+    fn orig(&self) -> Vector;
+
+    fn transp(&self) -> bool;
+
+    fn miroir(&self) -> bool;
+}
+
 struct Scene {
-    spheres: Vec<Sphere>,
+    shapes: Vec<Box<Shape + Sync>>,
     lumiere: usize,
     intensite_lumiere: f32,
 }
@@ -153,8 +177,7 @@ impl Vector {
     }
 }
 
-impl Sphere {
-
+impl Shape for Sphere {
     fn intersection(&self, r: &Ray, p: &mut Vector, n: &mut Vector, t: &mut f32) -> bool {
         // résoud a*t^2 + b*t + c = 0
 
@@ -192,19 +215,115 @@ impl Sphere {
 
     }
 
+    fn radius(&self) -> f32 {
+        self.radius
+    }
+
+    fn albedo(&self) -> Vector {
+        self.albedo.clone()
+    }
+
+    fn orig(&self) -> Vector {
+        self.orig.clone()
+    }
+
+    fn transp(&self) -> bool {
+        self.transp
+    }
+
+    fn miroir(&self) ->  bool {
+        self.miroir
+    }
+}
+
+impl Shape for Triangle {
+    fn intersection(&self, r: &Ray, p: &mut Vector, n: &mut Vector, t: &mut f32) -> bool {
+        let n_tmp = (self.B.clone() - self.A.clone()).cross(&(self.C.clone() - self.A.clone())).get_normalized();
+        let t_tmp = (self.C.clone() - r.orig.clone()).dot(&n.clone()) / r.dest.clone().dot(&n.clone());
+        if t_tmp < 0.0 {
+            return false;
+        }
+        let p_tmp = r.orig.clone() + r.dest.clone()*t_tmp;
+        let u = self.B.clone() - self.A.clone();
+        let v = self.C.clone() - self.A.clone();
+        let w = p.clone() - self.A.clone();
+
+        let m11 = u.get_norm2();
+        let m12 = u.clone().dot(&v.clone());
+        let m22 = v.get_norm2();
+        let detm = m11 * m22 - m12 * m12; // Matrice symétrique
+
+        let b11 = w.clone().dot(&u.clone());
+        let b21 = w.clone().dot(&v.clone());
+        let detb = b11 * m22 - b21 * m12;
+        let beta = detb / detm;
+
+        let g12 = b11;
+        let g22 = b21;
+        let detg = m11 * g22 - g12 * m12;
+        let gamma = detg / detm;
+
+        let alpha = 1. - beta - gamma;
+
+        // TODO: déplacer les conditions juste après les calculs des det et voir l'impact sur les perfs
+        if alpha < 0. || alpha > 1. {
+            return false;
+        }
+        if beta < 0. || beta > 1. {
+            return false;
+        }
+        if gamma < 0. || gamma > 1. {
+            return false;
+        }
+        if alpha + beta + gamma > 1. {
+            return false;
+        }
+
+        *t = t_tmp;
+        p.x = p_tmp.x;
+        p.y = p_tmp.y;
+        p.z = p_tmp.z;
+        n.x = n_tmp.x;
+        n.y = n_tmp.y;
+        n.z = n_tmp.z;
+        return true;
+    }
+
+    fn radius(&self) -> f32 {
+        0.
+    }
+
+    fn albedo(&self) -> Vector {
+        self.albedo.clone()
+    }
+
+    fn orig(&self) -> Vector {
+        Vector::new()
+    }
+
+    fn transp(&self) -> bool {
+        self.transp
+    }
+
+    fn miroir(&self) ->  bool {
+        self.miroir
+    }
 }
 
 impl Scene {
 
     fn add_sphere(&mut self, sphere: Sphere) {
-        self.spheres.push(sphere);
+        self.shapes.push(Box::new(sphere));
+    }
+    fn add_triangle(&mut self, triangle: Triangle) {
+        self.shapes.push(Box::new(triangle));
     }
 
     fn intersection(&self, r: &Ray, p: &mut Vector, n: &mut Vector, sphere_id: &mut usize, min_t: &mut f32) -> bool {
         let mut has_inter: bool = false;
         *min_t = 1e10;
 
-        for (i, s) in self.spheres.iter().enumerate() {
+        for (i, s) in self.shapes.iter().enumerate() {
             let mut t: f32 = 0.0;
             let mut local_p: Vector = Vector {x: 0.0, y: 0.0, z: 0.0};
             let mut local_n: Vector = Vector {x: 0.0, y: 0.0, z: 0.0};
@@ -231,6 +350,7 @@ fn main() {
     const X: i32 = 800;
     const Y: i32 = 800;
     const NRAYS: u16 = 600;
+    const NBOUNCES: u8 = 5;
     const FOV: f32 = 60.0*PI/180.0;
 
     let mut image = vec![0u8; (X*Y*3) as usize];
@@ -239,7 +359,6 @@ fn main() {
     let position_camera = Vector::new();
     let focus_distance = 60.0;
 
-    eprintln!("Création des paramètres : {:?}", time::now().tm_sec - time_start);
     let slum = Sphere { orig: position_lumiere, radius: 15.0, albedo: Vector { x: 1.0, y: 1.0, z: 1.0 }, miroir: false, transp: false };
     let s1 = Sphere { orig: Vector { x: 0.0, y: 0.0, z: -55.0 }, radius: 10.0, albedo: Vector { x: 1.0, y: 0.0, z: 1.0 }, miroir: false, transp: false };
     let s1bis = Sphere { orig: Vector { x: -15.0, y: 0.0, z: -35.0 }, radius: 10.0, albedo: Vector { x: 1.0, y: 0.0, z: 0.0 }, miroir: false, transp: true };
@@ -249,9 +368,12 @@ fn main() {
     let s4 = Sphere { orig: Vector { x: -2050.0, y: 0.0, z: 0.0 }, radius: 2000.0, albedo: Vector { x: 0.0, y: 1.0, z: 0.0 }, miroir: false, transp: false }; // left wall
     let s5 = Sphere { orig: Vector { x: 2050.0, y: 0.0, z: 0.0 }, radius: 2000.0, albedo: Vector { x: 0.0, y: 0.0, z: 1.0 }, miroir: false, transp: false }; // right wall
     let s6 = Sphere { orig: Vector { x: 0.0, y: 0.0, z: -2100.0 }, radius: 2000.0, albedo: Vector { x: 0.0, y: 1.0, z: 1.0 }, miroir: false, transp: false }; // back wall
+    let tri = Triangle { A: Vector { x: -10., y: -10., z: -20.}, B: Vector { x: 10., y: -10., z: -20.}, C: Vector { x: 0., y: 10., z: -20.}, albedo: Vector {x: 1., y: 0., z: 0.}, miroir: false, transp: false };
 
-    let mut scene = Scene { spheres: Vec::new(), lumiere: 0, intensite_lumiere: intensite_lumiere };
+    let mut scene = Scene { shapes: Vec::new(), lumiere: 0, intensite_lumiere: intensite_lumiere };
     scene.add_sphere(slum);
+
+    scene.add_triangle(tri);
     scene.add_sphere(s1);
     scene.add_sphere(s1bis);
     scene.add_sphere(s1ter);
@@ -260,21 +382,19 @@ fn main() {
     scene.add_sphere(s4);
     scene.add_sphere(s5);
     scene.add_sphere(s6);
-    eprintln!("Création de la scène : {:?}", time::now().tm_sec - time_start);
     let arc_image = Arc::new(Mutex::new(image));
 
     for i in 0..Y {
             (0..X).into_par_iter()
-                .for_each(|j| fill_image(i, j, &scene, NRAYS, X, Y, FOV, &position_camera, focus_distance, Arc::clone(&arc_image)));
+                .for_each(|j| fill_image(i, j, &scene, NRAYS, NBOUNCES, X, Y, FOV, &position_camera, focus_distance, Arc::clone(&arc_image)));
     }
 
-    eprintln!("Fin du calcul de l'image {:?}", time::now().tm_min - time_start_min);
 
     let image = arc_image.lock().unwrap();
     save_img("./image.bmp", &image, X as u32, Y as u32);
 }
 
-fn fill_image( i: i32, j: i32, scene: &Scene, n_rays: u16, x: i32, y: i32, fov: f32, position_camera: &Vector, focus_distance: f32, image: Arc<Mutex<Vec<u8>>>) {
+fn fill_image( i: i32, j: i32, scene: &Scene, n_rays: u16, n_rebonds: u8, x: i32, y: i32, fov: f32, position_camera: &Vector, focus_distance: f32, image: Arc<Mutex<Vec<u8>>>) {
     let mut color = Vector::new();
     for n in 0..n_rays {
 
@@ -303,7 +423,7 @@ fn fill_image( i: i32, j: i32, scene: &Scene, n_rays: u16, x: i32, y: i32, fov: 
         let new_origin = position_camera.clone() + Vector { x: dx_aperture, y: dy_aperture, z: 0.0};
         let r = Ray { orig: new_origin.clone(), dest: (destination.clone() - new_origin.clone()).get_normalized() };
 
-        color += get_color( &r, &scene, 5, true )/(n_rays as f32);
+        color += get_color( &r, &scene, n_rebonds, true )/(n_rays as f32);
     }
 
     {
@@ -331,11 +451,11 @@ fn get_color(r: &Ray, scene: &Scene, nbrebonds: u8, show_lights: bool) -> Vector
     if scene.intersection(&r, &mut p, &mut n, &mut id, &mut t) {
 
         if id == scene.lumiere {
-            let intensite_pixel = if show_lights { scene.spheres[scene.lumiere].albedo.clone()*scene.intensite_lumiere } else { Vector::new() };
+            let intensite_pixel = if show_lights { scene.shapes[scene.lumiere].albedo().clone()*scene.intensite_lumiere } else { Vector::new() };
             return intensite_pixel;
         } else {
 
-            if scene.spheres[id].miroir {
+            if scene.shapes[id].miroir() {
 
                 let direction_miroir = r.dest.clone() - n.clone()*n.clone().dot(&r.dest.clone())*2.0;
                 let rayon_miroir = Ray { orig: p + n*0.001, dest: direction_miroir };
@@ -344,7 +464,7 @@ fn get_color(r: &Ray, scene: &Scene, nbrebonds: u8, show_lights: bool) -> Vector
 
             } else {
 
-                if scene.spheres[id].transp {
+                if scene.shapes[id].transp() {
 
                     let mut n1 = 0.0;
                     let mut n2 = 0.0;
@@ -387,9 +507,9 @@ fn get_color(r: &Ray, scene: &Scene, nbrebonds: u8, show_lights: bool) -> Vector
 
                 } else {
 
-                    let axe_op = (p.clone() - scene.spheres[scene.lumiere].orig.clone()).get_normalized();
+                    let axe_op = (p.clone() - scene.shapes[scene.lumiere].orig().clone()).get_normalized();
                     let dir_aleatoire = axe_op.random_cos();
-                    let point_aleatoire = dir_aleatoire.clone() * scene.spheres[scene.lumiere].radius + scene.spheres[scene.lumiere].orig.clone() ;
+                    let point_aleatoire = dir_aleatoire.clone() * scene.shapes[scene.lumiere].radius() + scene.shapes[scene.lumiere].orig().clone() ;
                     let wi = (point_aleatoire.clone() - p.clone()).get_normalized();
                     let d_light2 = (point_aleatoire.clone() - p.clone()).get_norm2();
                     let np = dir_aleatoire.clone();
@@ -406,8 +526,8 @@ fn get_color(r: &Ray, scene: &Scene, nbrebonds: u8, show_lights: bool) -> Vector
 
                     } else {
 
-                        let brdf = scene.spheres[id].albedo.clone() / PI;
-                        let proba = axe_op.dot(&dir_aleatoire) / ( PI * scene.spheres[scene.lumiere].radius * scene.spheres[scene.lumiere].radius);
+                        let brdf = scene.shapes[id].albedo().clone() / PI;
+                        let proba = axe_op.dot(&dir_aleatoire) / ( PI * scene.shapes[scene.lumiere].radius() * scene.shapes[scene.lumiere].radius());
                         let j = 1.0 * np.dot(&(Vector::new() - wi.clone())) / d_light2;
                         let intensite_pixel =  brdf * scene.intensite_lumiere * 0f32.max(n.clone().dot(&wi.clone())) * j / proba;
 
@@ -417,7 +537,7 @@ fn get_color(r: &Ray, scene: &Scene, nbrebonds: u8, show_lights: bool) -> Vector
                     // Contribution de l'éclairage indirect
                     let direction_aleatoire = n.random_cos();
                     let rayon_aleatoire = Ray { orig: p + n*0.001, dest: direction_aleatoire };
-                    let albedo_local = scene.spheres[id].albedo.clone();
+                    let albedo_local = scene.shapes[id].albedo().clone();
                     let color = get_color( &rayon_aleatoire, &scene, nbrebonds -1, true )*albedo_local;
                     intensite_pixel += color;
                     return intensite_pixel;
